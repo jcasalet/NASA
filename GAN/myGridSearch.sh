4e87a5db7d334f3f8f027ef9673b5eadfb7401d4
James Casaletto <james.casaletto@gmail.com>
2021-11-16 22:42:36 -0800

    fixing max gamma

--- GAN/myGridSearch.sh
+++ GAN/myGridSearch.sh
@@ -2,22 +2,37 @@
 
 # python my_synthetic.py -g 0 -e 200 -ld 16 -bs 8 -nl 2 -hd 32 -lr 0.0005 -nb 5 -ng 250
 max_gamma=0
-for ld in 8 16
+epochs=30
+re='^[0-9]+([.][0-9]+)?$'
+for ld in 2 4
 do
-  for bs in 2 4 
+  for bs in 4 8
     do
-      for nl in 2 4
+      for nl in 4 8
       do
-        for hd in 64 128
+        for hd in 128 256
         do
-	  for lr in 1e-04 5e-04
+	  for lr in 5e-04 1e-03
 	  do
-             gamma=$(python ../../../NASA/GAN/my_synthetic.py -g 0 -e 100 -ld $ld -bs $bs -nl $nl -hd $hd -lr $lr -nb 5 -ng 0 | grep "Gamma(Dx, Dz):" | awk -F: '{print $2}' | xargs)
+             #python ../../../NASA/GAN/my_synthetic.py -g 0 -e 100 -ld $ld -bs $bs -nl $nl -hd $hd -lr $lr -nb 5 -ng 0 
+	     echo "new job: ld=$ld bs=$bs nl=$nl hd=$hd lr=$lr"
+             gamma=$(python ../../../NASA/GAN/my_synthetic.py -g 0 -e $epochs -ld $ld -bs $bs -nl $nl -hd $hd -lr $lr -nb 5 -ng 0 2>/dev/null | grep "Gamma(Dx, Dz):" | awk -F: '{print $2}' | xargs)
+	     if ! [[ $gamma =~ $re ]]
+	     then
+	        rm -rf checkpoints
+	        mkdir checkpoints
+		echo "got a nan ... continuing to next"
+                continue 
+ 	     fi
+	     echo "got a number $gamma ... seeing if it's max"
+	     gamma_ten=$(echo $gamma | cut -d. -f2)
              echo "gamma:" $gamma "ld:" $ld "bs:" $bs "nl:" $nl "hd:" $hd "lr:" $lr >> ./params.txt
-             if [ $gamma -gt $max_gamma ]
+             if [ $gamma_ten -gt $max_gamma ]
              then
+		echo "found max $gamma"
                 echo "gamma:" $gamma "ld:" $ld "bs:" $bs "nl:" $nl "hd:" $hd "lr:" $lr >> ./best_params.txt
-                max_gamma=$gamma
+		cp checkpoints/models/gen_liver.h5 MODELS/gamma_${gamma}_ld_${ld}_bs_${bs}_nl_${nl}_hd_${hd}_lr_${lr}.h5
+                max_gamma=$gamma_ten
              fi
            done
         done
