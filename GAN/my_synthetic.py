6d93acf20d68126fe4935cdc96dbda81e50c2dd2
James Casaletto <james.casaletto@gmail.com>
2021-11-18 10:47:59 -0800

    dont stdize

--- GAN/my_synthetic.py
+++ GAN/my_synthetic.py
@@ -839,7 +839,7 @@ def main():
                     theMin = val
 
         print('min = ' + str(theMin))'''
-        #x_gen_df = x_gen_df * expr_df_sd + expr_df_mean
+        x_gen_df = x_gen_df * expr_df_sd + expr_df_mean
         #x_gen_df = x_gen_df + abs(theMin)
         x_gen_df.to_csv(options.gendf_file, sep=',', header=True, index=True)
 
