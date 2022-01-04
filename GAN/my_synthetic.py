caf01118b96efd9fe4456199b28b62c98d421bb3
James Casaletto <james.casaletto@gmail.com>
2022-01-03 16:16:48 -0800

    unstdize and clip

--- GAN/my_synthetic.py
+++ GAN/my_synthetic.py
@@ -900,11 +900,11 @@ def main():
         expr_df_genes = expr_df.index
         # undo log transform
         x_gen = np.power(x_gen, np.e)
-        # undo standardization
-        x_gen = x_gen * expr_df.std() + expr_df.mean()
-        # remove any negative values
-        x_gen = np.clip(x_gen, 0, a_max=None)
         x_gen_df = pd.DataFrame(data=x_gen.T, index=expr_df_genes, columns=expr_df_samples)
+        # undo standardization
+        x_gen_df = x_gen_df * expr_df.std() + expr_df.mean()
+        # remove any negative values
+        x_gen_df = np.clip(x_gen_df, 0, a_max=None)
         x_gen_df.to_csv(options.gen_dir + '/gen.csv', sep=',', header=True, index=True)
 
     print('x.shape = ', x.shape)
