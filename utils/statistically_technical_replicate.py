57dd8cb98874799aee8ce3315afb22a2c2b5bf03
James Casaletto <james.casaletto@gmail.com>
2022-01-25 16:47:20 -0800

    refactor output name

--- utils/statistically_technical_replicate.py
+++ utils/statistically_technical_replicate.py
@@ -63,7 +63,7 @@ def main():
     expr_df = expr_df.drop(columns=['gene'])
     expr_df = np.clip(expr_df, 0, a_max=None)
     expr_df.insert(0, 'gene', genes)
-    outputFileNameSuffix = '__expanded_' + str(n) + '_' + str(var) + '.csv'
+    outputFileNameSuffix = '__expanded_' + str(n) + '_' + str(var) + str(colVal) + '.csv'
     expr_df.to_csv(expr_df_file.split('.')[0] + outputFileNameSuffix, index=False)
     meta_df.to_csv(meta_df_file.split('.')[0] + outputFileNameSuffix, index=False)
     print('new expr dims: ', str(expr_df.shape))
