5aa58da6a444325af286bddefda333b36f8056c1
James Casaletto <james.casaletto@gmail.com>
2022-03-14 14:05:41 -0700

    assert

--- GAN/my_synthetic.py
+++ GAN/my_synthetic.py
@@ -122,7 +122,7 @@ def correlations_list(x, y, corr_fn=pearson_correlation, kappa=1):
     :param y: Gene matrix 2. Shape=(nb_samples, nb_genes_2)
     :param corr_fn: correlation function taking x and y as inputs
     """
-    corr = cosine_similarity(x, y)
+    corr = pearson_correlation(x, y)
     result = upper_diag_list(corr)
     return result
     #return upper_diag_list(pearson_correlation(x, y, kappa))
