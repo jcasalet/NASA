889c754df99219bcf3c68dbf9c829d56f7ad4a4b
jcasalet <james.casaletto@gmail.com>
2022-06-15 15:19:48 -0700

    plot hists

--- utils/analyze_results.py
+++ utils/analyze_results.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from statistics import median, mean
 import sys
+import matplotlib.pyplot as plt
 
 results_json_file = sys.argv[1]
 
@@ -9,7 +10,9 @@ results=pd.read_json(results_json_file)['results']
 for i in range(len(results)):
 	if 'method' in results.iloc[i].keys():
 		print(results.iloc[i]['method'])
-		print('min coeff = ', min(results.iloc[i]['coefficients']))
-		print('max coeff = ', max(results.iloc[i]['coefficients']))
-		print('mean coeff = ', mean(results.iloc[i]['coefficients']))
-
+		coefs=list(results.iloc[i]['coefficients'])
+		print('min coeff = ', min(coefs)
+		print('max coeff = ', max(coefs)
+		print('mean coeff = ', mean(coefs)
+		plt.hist(coefs)
+		plt.show()
