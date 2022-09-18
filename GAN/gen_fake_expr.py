e6acc18754cbab7fd9cb797fb12ba933bd2cbb37
jcasalet <james.casaletto@gmail.com>
2022-09-18 13:06:24 -0700

    fix stdize call

--- GAN/gen_fake_expr.py
+++ GAN/gen_fake_expr.py
@@ -533,15 +533,30 @@ def my_find_mostvaried(df, n):
     slicedDF = df[:,indices]
     return slicedDF, indices
 
-def my_standardize(self, df, transpose=True):
+def my_standardize(df):
     # this method requires df in samples x genes
-    df = df.T
+    # this method requires df in samples x genes
+    if 'gene' in list(df.columns):
+        transpose = True
+        df = transpose_df(df, cur_index_col='gene', new_index_col='sample')
+    numeric_cols = list(df.columns[1:])
+    samples = list(df['sample'])
+    df.drop(columns=['sample'], inplace=True)
     df = df.apply(zscore, axis=0)
+    # self.expressionDF = (self.expressionDF - self.expressionDF.mean(axis=1)) / self.expressionDF.std(axis=1)
+    df['sample'] = samples
+    df = df[['sample'] + numeric_cols]
     if transpose:
-        return self.transpose_df(df, cur_index_col='sample', new_index_col='gene')
+        return transpose_df(df, cur_index_col='sample', new_index_col='gene')
     else:
         return df
 
+def transpose_df(df, cur_index_col, new_index_col):
+    df = df.set_index(cur_index_col).T
+    df.reset_index(level=0, inplace=True)
+    cols = [new_index_col] + list(df.columns)[1:]
+    df.columns=cols
+    return df
 
 def my_prep_data(n, expr_df, info_df, seed, use_meta_cols, train_percent):
 
@@ -595,7 +610,7 @@ def my_prep_data(n, expr_df, info_df, seed, use_meta_cols, train_percent):
     # 2. standardize expression data
     #x = (x - x.mean()) / x.std()
     #x = (x - x.mean()) / x.std()
-    x = my_standardize(df=x, transpose=True)
+    x = my_standardize(df=x)
 
     # 3. transpose matrix (don't need to do this step if in previous step you did the transpose
     #x = x.T
