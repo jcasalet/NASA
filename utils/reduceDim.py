459458920cf471133e2241a7baeecd0928cab821
jcasalet <james.casaletto@gmail.com>
2022-09-18 10:37:33 -0700

    set =0 logic

--- utils/reduceDim.py
+++ utils/reduceDim.py
@@ -81,10 +81,10 @@ def main():
 	if 'index' in list(df.columns):
 		df.drop(columns=['index'], inplace=True)
 	outputFileName = exprFile.split('.csv')[0] + "__reduced_" +  \
-					 "_a=" + str(alpha) + \
-					 "_s=" + str(sigma) +  \
-					 "_d=" + str(delta) + \
-					 "_n=" + str(n)  + \
+					 "_a\=" + str(alpha) + \
+					 "_s\=" + str(sigma) +  \
+					 "_d\=" + str(delta) + \
+					 "_n\=" + str(n)  + \
 					 ".csv"
 
 	print('filename is ', outputFileName)
