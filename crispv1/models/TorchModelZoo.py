1a1bfe8f7092452b485d7f458e42addfb721a56e
jcasalet <james.casaletto@gmail.com>
2022-10-16 13:59:13 -0700

    use softmax at last layer

--- crispv1/models/TorchModelZoo.py
+++ crispv1/models/TorchModelZoo.py
@@ -84,3 +84,46 @@ class MLP2(torch.nn.Module):
         out = input.view(input.shape[0], self.input_dim)
         out = self._main(out)
         return out
+
+
+class MLP3(torch.nn.Module):
+    def __init__(self, flags, input_dim, output_dim):
+        super(MLP3, self).__init__()
+        hid_dim1 = flags['hidden_dim']
+        hid_dim2 = int(hid_dim1 / 2)
+        hid_dim3 = int(hid_dim2 / 2)
+        dropout_p_low = 0.4
+        dropout_p_high = 0.4
+
+        lin1 = torch.nn.Linear(input_dim, hid_dim1, bias=True)
+        d1 = torch.nn.Dropout(dropout_p_low)
+        lin2 = torch.nn.Linear(hid_dim1, hid_dim1, bias=True)
+        d2 = torch.nn.Dropout(dropout_p_low)
+        lin3 = torch.nn.Linear(hid_dim1, hid_dim2, bias=True)
+        d3 = torch.nn.Dropout(dropout_p_low)
+        lin4 = torch.nn.Linear(hid_dim2, hid_dim2, bias=True)
+        d4 = torch.nn.Dropout(dropout_p_low)
+        lin5 = torch.nn.Linear(hid_dim2, hid_dim3, bias=True)
+        d5 = torch.nn.Dropout(dropout_p_low)
+        lin6 = torch.nn.Linear(hid_dim3, hid_dim3, bias=True)
+        d6 = torch.nn.Dropout(dropout_p_high)
+        lin7 = torch.nn.Linear(hid_dim3, output_dim, bias=True)
+
+        for lin in [lin1, lin2, lin3, lin4, lin5, lin6, lin7]:
+            torch.nn.init.xavier_uniform_(lin.weight)
+            torch.nn.init.zeros_(lin.bias)
+        self._main = torch.nn.Sequential(lin1, torch.nn.ReLu(True), d1,
+                                         lin2, torch.nn.ReLu(True), d2,
+                                         lin3, torch.nn.ReLu(True), d3,
+                                         lin4, torch.nn.ReLu(True), d4,
+                                         lin5, torch.nn.ReLu(True), d5,
+                                         lin6, torch.nn.ReLu(True), d6,
+                                         lin7, torch.nn.Softmax()
+                                         )
+        self.input_dim = input_dim
+        self.output_dim = output_dim
+
+    def forward(self, input):
+        out = input.view(input.shape[0], self.input_dim)
+        out = self._main(out)
+        return out
