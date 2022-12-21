043aa8f98632901bbf83b9894db0c6806d18558e
jcasalet <james.casaletto@gmail.com>
2022-12-21 12:12:30 -0800

    indent error

--- crispv1/models/LinearInvariantRiskMinimization.py
+++ crispv1/models/LinearInvariantRiskMinimization.py
@@ -14,7 +14,7 @@ class LinearInvariantRiskMinimization(object):
         self.output_dim =  environment_datasets[0].get_output_dim()
         self.test_dataset = test_dataset
         self.args = args
-	self.args['output_data_regime'] = 'binary'
+        self.args['output_data_regime'] = 'binary'
         self.logging_iteration = args.get('logging_iteration', 200)
         self.loss_per_iteration = []
         self.acc_per_iteration = []
