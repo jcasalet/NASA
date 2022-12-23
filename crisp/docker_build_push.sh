2010598c6cd2a773f3c2b4f5d90eb098fad5529d
jcasalet <james.casaletto@gmail.com>
2022-12-23 10:30:52 -0800

    remove crisp

--- crisp/docker_build_push.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-if docker build -t ah-causal-ensemble-docker .  ; then
-  docker tag ah-causal-ensemble-docker gcr.io/fdl-astronaut-health/ah-causal-ensemble-docker
-  docker push gcr.io/fdl-astronaut-health/ah-causal-ensemble-docker
-
-  docker tag ah-causal-ensemble-docker registry.gitlab.com/frontierdevelopmentlab/astronaut-health/ah-causal-ensemble
-  docker push registry.gitlab.com/frontierdevelopmentlab/astronaut-health/ah-causal-ensemble
-fi
